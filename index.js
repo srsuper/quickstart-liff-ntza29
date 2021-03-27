cb385023bf4d3ee33a304611c74b08110859eaca
srsuper <aot5310@gmail.com>
2021-03-27 18:44:55 +0700

    2 files modified

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 // Import stylesheets
-import "./style.css"
+import "./style.css";
 
 // Body element
-const body = document.getElementById('body')
+const body = document.getElementById("body");
 
 // Button elements
-const btnSend = document.getElementById("btnSend")
-const btnClose = document.getElementById("btnClose")
-const btnShare = document.getElementById("btnShare")
-const btnLogIn = document.getElementById("btnLogIn")
-const btnLogOut = document.getElementById("btnLogOut")
-const btnScanCode = document.getElementById("btnScanCode")
-const btnOpenWindow = document.getElementById("btnOpenWindow")
+const btnSend = document.getElementById("btnSend");
+const btnClose = document.getElementById("btnClose");
+const btnShare = document.getElementById("btnShare");
+const btnLogIn = document.getElementById("btnLogIn");
+const btnLogOut = document.getElementById("btnLogOut");
+const btnScanCode = document.getElementById("btnScanCode");
+const btnOpenWindow = document.getElementById("btnOpenWindow");
 
 // Profile elements
-const email = document.getElementById("email")
-const userId = document.getElementById("userId")
-const pictureUrl = document.getElementById("pictureUrl")
-const displayName = document.getElementById("displayName")
-const statusMessage = document.getElementById("statusMessage")
+const email = document.getElementById("email");
+const userId = document.getElementById("userId");
+const pictureUrl = document.getElementById("pictureUrl");
+const displayName = document.getElementById("displayName");
+const statusMessage = document.getElementById("statusMessage");
 
 // QR element
-const code = document.getElementById("code")
-const friendShip = document.getElementById("friendShip")
+const code = document.getElementById("code");
+const friendShip = document.getElementById("friendShip");
 
 async function main() {
   // Initialize LIFF app)
-  
+  await liff.init({ liffId: "1655602828-Ok0eDZWP" });
   // Try a LIFF function
+  switch (liff.getOS()) {
+    case "android":
+      body.style.backgroundColor = "#d1f5d3";
+      break;
+    case "ios":
+      body.style.backgroundColor = "#eeeeee";
+      break;
+  }
 }
-main()
+main();
